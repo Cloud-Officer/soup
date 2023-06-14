e177bdf4b91cd741612828b5d2ae67b73feb3084
Yves Desgagne <yves@cloudofficer.ca>
2023-06-14 17:39:21 -0400

    Made version optional for pip packages

--- lib/soup/parsers/pip.rb
+++ lib/soup/parsers/pip.rb
@@ -16,9 +16,7 @@ module SOUP
         line = line.slice(0, line.index(';')) if line.include?(';')
         pip_package, version = line.strip.split(/==/)
 
-        next if pip_package.strip.empty?
-
-        next if version.strip.empty?
+        next if pip_package&.strip&.empty?
 
         puts("Checking #{pip_package} #{version}...")
         response = HTTParty.get("https://pypi.python.org/pypi/#{pip_package}/json")
