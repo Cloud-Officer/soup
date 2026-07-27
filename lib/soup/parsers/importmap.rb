d4ed663a2410a17d7b6e3ee6055b0d64868fb0c8
Yves Desgagné <yves@cloudofficer.ca>
2026-07-27 14:42:32 -0400

    #386: use npm_registry_license helper in the importmap parser

--- lib/soup/parsers/importmap.rb
+++ lib/soup/parsers/importmap.rb
@@ -72,15 +72,12 @@ module SOUP
       package_details = lookup_npm_registry_version(payload, name: name, version: resolved_version)
       return if package_details.nil?
 
-      raw_license = package_details['license']
-      license = raw_license.is_a?(Hash) ? raw_license['type'].to_s : raw_license.to_s
-
       build_package(
         name: name,
         file: file,
         language: 'JS',
         version: resolved_version,
-        license: license,
+        license: npm_registry_license(package_details['license']),
         description: Package.sanitize_description(package_details['description'], strip_markdown: true),
         website: package_details['homepage'],
         dependency: false
